29822d5789abc46f368fa941a8c1b8d400d284f6
SynxEU <126554945+SynxEU@users.noreply.github.com>
2023-12-11 08:29:43 +0100

    Add project files.

---
+++ regnvejrStatestik/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace regnvejrStatestik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
